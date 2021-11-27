1a56145b1cdfb0fc4c8a2309b26a2f6ac4294b66
Muaz <Muaz>
2021-11-27 10:20:45 +0300

    Backend testler ile Eklendi

--- backend/src/main/java/com/todolist/controller/TodoController.java
+++ backend/src/main/java/com/todolist/controller/TodoController.java
@@ -1,6 +1,6 @@
 package com.todolist.controller;
 
-import com.todolist.model.TodoDTO;
+import com.todolist.model.Todo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +15,9 @@ public class TodoController {
     private final TodoService todoService;
 
     @PostMapping
-    public ResponseEntity kaydet(@RequestBody TodoDTO todoDTO){
+    public ResponseEntity kaydet(@RequestBody Todo todo){
         try {
-            todoService.kaydet(todoDTO);
+            todoService.kaydet(todo);
             return ResponseEntity.ok().build();
         } catch (Exception e){
             throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
--- backend/src/main/java/com/todolist/controller/TodoService.java
+++ backend/src/main/java/com/todolist/controller/TodoService.java
@@ -1,14 +1,12 @@
 package com.todolist.controller;
 
-import com.todolist.model.TodoDTO;
-import com.todolist.model.TodoMapper;
+import com.todolist.model.Todo;
 import com.todolist.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +15,12 @@ public class TodoService {
     private final TodoRepository todoRepository;
 
     @Transactional
-    public void kaydet(TodoDTO todoDTO){
-        this.todoRepository.save(TodoMapper.INSTANCE.DTOtoEntity(todoDTO));
+    public void kaydet(Todo todo){
+        this.todoRepository.save(todo);
     }
 
     @Transactional(readOnly = true)
-    public List<TodoDTO> getAllTodo(){
-        return this.todoRepository.findAll().stream().map( todo -> TodoMapper.INSTANCE.entityToDTO(todo)).collect(Collectors.toList());
+    public List<Todo> getAllTodo(){
+        return this.todoRepository.findAll();
     }
 }
